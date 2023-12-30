943c055cff588753840fb2c0803ca6cf1770016c
yJoelhenry7 <20pa1a5458@vishnu.edu.in>
2023-12-30 20:03:38 +0530

    Adding controllers and routes

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const express = require('express');
 const app = express();  
 const PORT = process.env.PORT || 3000
 
-app.get('/', function (req, res) {  
-  res.send("response sended")
-});  
+// ---------------------MiddleWares----------------------------------------------------
+// parse requests of content-type - application/json
+app.use(express.json())
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }))
 
+// ---------------------Routes-------------------------------------------------
+const Router = require('./routes/Router')
+app.use('/',Router)
 
 app.listen(PORT, function () {  
     console.log('Node server is running..');  
